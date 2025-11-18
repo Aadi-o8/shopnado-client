ac4818bbce51e31b354460ff8172674428c27c2e
Aadi-o8 <adityap7160@gmail.com>
2025-11-18 16:20:51 +0000

    mobile responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='flex flex-col sm:flex-row mx-10 '>
         {/* LEFT SECTION */}
-        <div className='border-l-2 border-b-2 border-t-2 border-gray-600 rounded-l-2xl w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0 bg-white'>
+        <div className='border-l-2 border-b-2 border-t-2 border-gray-600 rounded-2xl sm:rounded-r-none rounded-b-none w-full border-r-2 sm:border-r-0 sm:w-1/2 flex items-center justify-center py-10 sm:py-0 bg-white'>
             <div className=' text-[#414141]'>
                 <div className=' flex items-center gap-2'>
                     <p className='w-8 md:w-11 h-0.5 bg-[#414141]'></p>
@@ -20,7 +20,7 @@ const Hero = () => {
         </div>
         
         {/* RIGHT SECTION */}
-        <div className='overflow-hidden w-full sm:w-1/2 flex items-center justify-center border-y-2 border-r-2 rounded-r-2xl border-gray-600 py-10 sm:py-0'>
+        <div className='overflow-hidden w-full sm:w-1/2 flex items-center justify-center border-b-2 sm:border-y-2 border-x-2 sm:border-l-2 rounded-t-none sm:rounded-t-2 sm:rounded-l-none rounded-2xl border-gray-600 sm:py-0'>
             <img src={latest} className='w-full' alt="" />
         </div>
     </div>
